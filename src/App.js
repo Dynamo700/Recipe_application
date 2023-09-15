c44cdc4bac076e48ab0a3a82e22f883a38340135
Nick <nickq4504@gmail.com>
2023-09-15 16:09:38 -0230

    Nav bar created which the user can use to navigate to the view page and add page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
-import logo from './logo.svg';
 import './App.css';
+import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 export function View() {
   return(
     <div>
+      <nav>
+        <Link to="/">View</Link>
+        <Link to="/add">Add</Link>
+      </nav>
       <h1>Recipe viewer</h1>
+      <h3>recipe name</h3>
+      <h3>recipe ingreidents</h3>
+      <h3>recipe directions</h3>
+      <h3>image of recipe</h3>
     </div>
   );
 }
@@ -13,6 +21,10 @@ export function View() {
 export function Add() {
   return (
     <div>
+      <nav>
+        <Link to="/">View</Link>
+        <Link to="/add">Add</Link>
+      </nav>
       <h1>Add</h1>
     </div>
   )
