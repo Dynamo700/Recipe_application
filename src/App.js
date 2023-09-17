42b76d9a030aa0651ce86ef549376ddff1ded51f
Nick <nickq4504@gmail.com>
2023-09-17 18:56:00 -0230

    image file examples as well as image file folder created. Two recipe examples have been created and hardcoded as part of a test to view recipes on the site.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import { useState, useEffect } from 'react';
 
 export function View() {
 
-  let recipeData = [ {name: "pizza", ingreidents: "cheese, dough, pizza sauce, pepperoni", directions: "cook at 350", description: "round"},
-                      {name: "cupcake", ingreidents: "flour, sugar, egg, water ", direcations: "cook at 400", description: "cupcake"}];
+  let recipeData = [ {name: "pizza", 
+                      ingreidents: "cheese, dough, pizza sauce, pepperoni", 
+                      directions: "cook at 350", 
+                      description: "round",
+                      image: "./images/pizza.jpg"},
+                      {name: "cupcake", 
+                      ingreidents: "flour, sugar, egg, water ", 
+                      directions: "cook at 400", 
+                      description: "cupcake",
+                      image: "./images/cupcake.jpg"}];
 
 
   const [recipes, setRecipes] = useState(recipeData);
@@ -18,7 +26,14 @@ export function View() {
 
   return(
     recipes.map( (recipes, i)=> {
-      return <h3>{recipes.name}</h3>
+      return (<div>
+      <h3>{recipes.name}</h3>
+      <h3>{recipes.ingreidents}</h3>
+      <h3>{recipes.directions}</h3>
+      <h3>{recipes.description}</h3>
+      <img height={300} src={recipes.image} />
+      </div>
+      )
      
     })
   )
