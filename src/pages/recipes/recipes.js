ee0d66f4fb7eddc0ca324bbd9813c3ab266a6d02
Nick <nickq4504@gmail.com>
2023-09-21 19:53:55 -0230

    Data now saves to a local storage

--- src/pages/recipes/recipes.js
+++ src/pages/recipes/recipes.js
@@ -4,9 +4,11 @@ export function RecipeList( {Recipes} ){
             console.log(recipe)
             return (
             <>
-                <Recipe recipeName={recipe.name} recipeIngreident={recipe.ingreidents} recipeDirections={recipe.directions} recipeImage={recipe.image}/>
-                
-
+                <Recipe recipeName={recipe.name} 
+                recipeIngredient={recipe.ingredient} 
+                recipeDirections={recipe.directions} 
+                recipeDescription={recipe.description} 
+                recipeImage={recipe.image}/>
             </>
             )
             
@@ -17,14 +19,15 @@ export function RecipeList( {Recipes} ){
     )
 }
 
-function Recipe( {recipeName, recipeIngreident, recipeDirections, recipeImage} ) {
+function Recipe( {recipeName, recipeIngredient, recipeDirections, recipeDescription, recipeImage} ) {
 
     return (
         <>
-        <h3>{recipeName}</h3>
-        <h3>{recipeIngreident}</h3>
-        <h3>{recipeDirections}</h3>
-        <img src={recipeImage} width="300" height="300"/>
+        <h3>Name: {recipeName}</h3>
+        <h3>Ingredients: {recipeIngredient}</h3>
+        <h3>Directions: {recipeDirections}</h3>
+        <h3>Description: {recipeDescription}</h3>
+        <img src={recipeImage} width="350" height="350"/>
         </>
         
         
